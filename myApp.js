e778c6cd51f19bf5caf792e8009dc3d1824e6f4c
Thanh <tnkchaseme@gmail.com>
2022-08-14 18:21:44 -0500

    add chain middle ware

--- myApp.js
+++ myApp.js
@@ -12,6 +12,20 @@ app.use(function middleware(req, res, next) {
   });
 
 
+// cai nay vua chay request vua chay middleware check cung luc cho 1 cai 
+//thay vi tach ra function rieng va goi no theo kieu app.get("/now",function,(req,res))
+  app.get("/now",(req, res, next) => {
+    req.time = new Date().toString();
+    next();
+    },
+    (req, res) => {
+      res.send({
+        time: req.time
+      });
+    }
+  );
+
+
 app.get('/', function(req, res) {
     res.sendFile(__dirname+'/views/index.html')
 })
