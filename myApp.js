3b0a0f6d75e3da8bfda87fe7136ff48ecdd254f8
Thanh <tnkchaseme@gmail.com>
2022-08-14 19:10:50 -0500

    add middleware to app get

--- myApp.js
+++ myApp.js
@@ -5,23 +5,27 @@ const path = require('path')
 app.use('/public', express.static(path.join(__dirname, 'public')))
 
 // cai nay chay moi luc server dc nhan request
-app.use(function middleware(req, res, next) {
-    var string = req.method + " " + req.path + " - " + req.ip;
-    console.log(string)
-    next();
-  });
+// app.use(function middleware(req, res, next) {
+//     var string = req.method + " " + req.path + " - " + req.ip;
+//     console.log(string)
+//     next();
+//   });
 
 
 // cai nay vua chay request vua chay middleware check cung luc cho 1 cai 
 //thay vi tach ra function rieng va goi no theo kieu app.get("/now",function,(req,res))
-  app.get("/now",(req, res, next) => {
+app.get(
+  "/now",
+  (req, res, next) => {
     req.time = new Date().toString();
     next();
-    (req,res) => {
-        res.send({'time':req.time})
-    }
-} 
-  );
+  },
+  (req, res) => {
+    res.send({
+      time: req.time
+    });
+  }
+);
 
 
 app.get('/', function(req, res) {
